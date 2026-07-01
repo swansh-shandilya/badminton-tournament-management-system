e59a8e37185ba062ac3fc32c813381f1cee68c63
swansh-shandilya <swanshgshandilya@gmail.com>
2026-07-01 22:53:43 +0530

    added byes variable

--- code/main.py
+++ code/main.py
@@ -2,6 +2,7 @@ import math
 nop=int(input("enter number of players: "))
 num_slots = 2 ** math.ceil(math.log2(nop))
 data={}
+byes=num_slots-nop
 for i in range(1,nop+1):
     ply=input("enter the name of the player: ")
     lvl=int(input("enter number for level/experince of player(beginner(1),intermidiate(2),professional(3)): "))
@@ -9,7 +10,7 @@ for i in range(1,nop+1):
 sorted_data=dict(sorted(data.items(), key=lambda item: item[1], reverse=True))
 seed=list(sorted_data)
 name={i + 1: player for i, player in enumerate(seed)}
-slot=[] 
+slot=[]  
 brackets=[]       
 for i in range(1,math.ceil(len(name)/2)+1):
     val=(len(name)+1)- i
@@ -20,3 +21,4 @@ for i in range(0, len(unique_slots), 2):
         slot.append((unique_slots[i], unique_slots[i+1]))
     else:
         slot.append((unique_slots[i], None))        
+print(slot)
