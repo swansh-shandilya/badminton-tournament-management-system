2fc9a8bb6e982b864146641b6e5324835eb776af
swansh-shandilya <swanshgshandilya@gmail.com>
2026-07-02 17:29:44 +0530

    Complete single-elimination tournament logic

--- code/main.py
+++ code/main.py
@@ -22,14 +22,11 @@ for i in range(0, num_slots, 2):
     pairs.append((name[seed_order[i]], name[seed_order[i+1]]))
 bye=[]
 match=[]
-print(pairs)
 for i in pairs:
     if None in i:
         bye.append(i[0])
     else:
         match.append(i)    
-print(bye)
-print(match)
 winners=[]
 for i in match:
     while True:
@@ -39,6 +36,30 @@ for i in match:
             break
         else:
             print("Invalid input.")    
-print(winners)
-alive_players=bye+winners
-print(alive_players)    
+alive_players=bye+winners 
+seed_order.clear()
+seed_order = [1]
+while len(seed_order) < len(alive_players):
+    m = len(seed_order) * 2
+    seed_order = [x for s in seed_order for x in (s, m + 1 - s)]
+alive_players = [alive_players[seed_order[i] - 1] for i in range(len(seed_order))]
+match=[]
+winners=[]
+while len(alive_players)>1:
+    for i in range(0,len(alive_players),2):
+        if i+1<len(alive_players):
+            match.append((alive_players[i],alive_players[i+1]))
+    for i in match:
+        while True:
+            inp=input("enter the winner of the match betweeen {} and {}: ".format(i[0],i[1]))
+            if inp==i[0] or inp==i[1]:
+                winners.append(inp)
+                break
+            else:
+                print("Invalid input.")                      
+    alive_players.clear()
+    alive_players.extend(winners)
+    match.clear()
+    winners.clear()
+if len(alive_players)==1:
+    print("The winner of the tournament is: {}".format(alive_players[0]))  
