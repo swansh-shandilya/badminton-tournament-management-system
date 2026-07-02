9b33990bf076f4f2198704d4f6b27db38bdcac94
swansh-shandilya <swanshgshandilya@gmail.com>
2026-07-02 16:36:07 +0530

    improved seeding and bye allocation.

--- code/main.py
+++ code/main.py
@@ -2,7 +2,6 @@ import math
 nop=int(input("enter number of players: "))
 num_slots = 2 ** math.ceil(math.log2(nop))
 data={}
-byes=num_slots-nop
 for i in range(1,nop+1):
     ply=input("enter the name of the player: ")
     lvl=int(input("enter number for level/experince of player(beginner(1),intermidiate(2),professional(3)): "))
@@ -11,14 +10,35 @@ sorted_data=dict(sorted(data.items(), key=lambda item: item[1], reverse=True))
 seed=list(sorted_data)
 name={i + 1: player for i, player in enumerate(seed)}
 slot=[]  
-brackets=[]       
-for i in range(1,math.ceil(len(name)/2)+1):
-    val=(len(name)+1)- i
-    brackets.extend((name[i],name[val]))
-unique_slots = list(dict.fromkeys(brackets))
-for i in range(0, len(unique_slots), 2):
-    if i+1<len(unique_slots):
-        slot.append((unique_slots[i], unique_slots[i+1]))
+brackets=[]  
+for i in range(nop + 1, num_slots + 1):
+    name[i] = None
+seed_order = [1]
+while len(seed_order) < num_slots:
+    m = len(seed_order) * 2
+    seed_order = [x for s in seed_order for x in (s, m + 1 - s)]
+pairs = []
+for i in range(0, num_slots, 2):
+    pairs.append((name[seed_order[i]], name[seed_order[i+1]]))
+bye=[]
+match=[]
+print(pairs)
+for i in pairs:
+    if None in i:
+        bye.append(i[0])
     else:
-        slot.append((unique_slots[i], None))        
-print(slot)
+        match.append(i)    
+print(bye)
+print(match)
+winners=[]
+for i in match:
+    while True:
+        inp=input("enter the winner of the match betweeen {} and {}: ".format(i[0],i[1]))
+        if inp==i[0] or inp==i[1]:
+            winners.append(inp)
+            break
+        else:
+            print("Invalid input.")    
+print(winners)
+alive_players=bye+winners
+print(alive_players)    
